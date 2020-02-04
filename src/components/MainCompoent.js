d6e958c638a06df2d37d98cf70d098a1d9ebbacb
Yanzhen Wang <yanzhenwang0328@gmail.com>
2020-02-04 12:47:47 -0500

    Single page application

--- src/components/MainCompoent.js
+++ src/components/MainCompoent.js
@@ -1,11 +1,14 @@
 
 import React, {Component} from 'react';
 import Menu from './MenuComponent';
-import DishDetail from './DishdetailComponent';
 import {DISHES} from '../shared/dishes';
+import {COMMENTS} from '../shared/comments';
+import {LEADERS} from '../shared/leaders';
+import {PROMOTIONS} from '../shared/promotions';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import { Switch, Route, Redirect} from 'react-router-dom';
 
 
@@ -15,6 +18,9 @@ class Main extends Component {
     super(props);
     this.state = {
       dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS,
       selectedDish: null
     };
   }
@@ -25,22 +31,21 @@ class Main extends Component {
   render(){
    const HomePage = () =>{
        return(
-           <Home/>
+           <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}                
+                 leader={this.state.leaders.filter((leader) => leader.featured)[0]} 
+                 promotion ={this.state.promotions.filter((promo) => promo.featured)[0]}/>
        );
    }
   return (
     <div>
        <Header></Header>
      
-       <Switch>
+        <Switch>
               <Route path='/home' component={HomePage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+              <Route exact path='/contactus' component={() => <Contact />} />
               <Redirect to="/home" />
-          </Switch>
-        {/* <Menu dishes ={this.state.dishes}
-            onClick={(dishId) => this.onDishSelected(dishId)}></Menu>
-        <DishDetail
-                dish={this.state.dishes.filter((dish) =>dish.id === this.state.selectedDish )[0]}></DishDetail>  */}
+        </Switch>
         <Footer></Footer>
     </div>
   );
